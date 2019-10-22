e13539798cbed5697974b852e1cb2f988c3deb15
Librebowski <Librebowski@gmail.com>
2019-10-22 18:51:23 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,44 @@
+Commands:
+
+ $ cd Desktop
+
+ $ wget https://raw.githubusercontent.com/Librebowski/DS4DRV_Easy/blob/master/ds4drv.sh
+
+ $ chmod +x DS4DRV.sh
+
+ $ sudo ./DS4DRV.sh
+ 
+Authenticate with your password and you are good to go.
+
+
+#Install script for DS4DRV by: Librebowski.
+
+#For use with all Ubuntu and Debian variants.
+
+https://github.com/Librebowski/DS4DRV_Easy/blob/master/ds4drv.sh
+
+
+#This script enables and configures DS4DRV with X-Box and PlayStation configurations with the ability to switch FAST between the two.
+
+
+Instructions for pairing:
+
+ Step 1: Pair controler in the Bluetooth Manager on the top bar and make sure your first step is "Add" or "Add device".
+
+ Step 2: once paired correctly proceed to the next step.
+
+ Step 3: Now its kinda paired but not really (dont worry you dont have to do that last step everytime).
+
+ Step 4: Open Terminal and type: ds4drv and hit enter, hold Option and the middle button on the controller once the controller starts flashing you have finished the pairing process.
+
+ Step 5: (Optional) switch to XBox mode by holding R1 L1 and the middle button. This mode offer more compatibility in games.
+
+
+Video guide: https://www.youtube.com/watch?v=y8PFwXfDSk0
+
+ Have fun I hope you guys enjoy Gaming on Linux! Welcome to the community which is what makes all this possible.
+ 
+ Special thanks to: https://github.com/chrippa/ds4drv/
+ 
+-Librebowski
+ 
---
+++ ds4drv.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#Install script for DS4DRV by: Librebowski.
+
+echo " "
+echo "***Welcome to Librebowskis DS4DRV install script intended for Debian, Ubuntu and Mint***"
+sleep 2
+echo " "
+echo "Now installing dependencies..."
+echo " "
+sleep 3
+echo "This may take up to a few minutes, please be patient."
+echo " "
+sleep 5
+sudo apt-get update 
+#uncomment below line to enable upgrade as this may cause the video driver to need to be reinstalled.
+#sudo apt-get -y upgrade
+sudo apt-get install python-pip
+sudo -H pip install --upgrade pip
+echo " "
+echo "Now we will install DS4DRV..."
+echo " "
+sleep 5
+sudo -H pip install ds4drv
+cd ~/Downloads
+	wget https://raw.githubusercontent.com/Librebowski/DS4DRV_Easy/master/50-ds4drv.rules
+	sudo cp ~/Downloads/50-ds4drv.rules /etc/udev/rules.d/
+	sudo udevadm control --reload-rules
+	sudo udevadm trigger
+	cd ~/.config
+	wget https://raw.githubusercontent.com/Librebowski/DS4DRV_Easy/master/ds4drv.conf
+echo "Driver is now properly installed, thank you for using my script!"
+sleep 4
+echo " "
+echo " "
+echo "For detailed instructions on how to start and pair the driver please consult the the readme..."
+sleep 8
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+sleep 1
+echo "Have a nice day."
+sleep 3
